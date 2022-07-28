b4f9522dbd42e33f9d306dbc12baf9b318ee589e
melih022 <110200865+melih022@users.noreply.github.com>
2022-07-29 00:42:16 +0300

    Update stats.py

--- YukkiMusic/plugins/tools/stats.py
+++ YukkiMusic/plugins/tools/stats.py
@@ -281,21 +281,21 @@ async def overall_stats(client, CallbackQuery, _):
     else:
         ass = "No"
     cm = config.CLEANMODE_DELETE_MINS
-    text = f"""**Bot's Stats and Information:**
-**Imported Modules:** {mod}
-**Served Chats:** {served_chats} 
-**Served Users:** {served_users} 
-**Blocked Users:** {blocked} 
-**Sudo Users:** {sudoers} 
+    text = f"""**Bot'un İstatistikleri ve Bilgileri:**
+**İçe Aktarılan Modüller:** {mod}
+**Sunulan Sohbetler:** {served_chats} 
+**Hizmet Verilen Kullanıcılar:** {served_users} 
+**Engellenmiş kullanıcılar:** {blocked} 
+**Sudo Kullanıcıları:** {sudoers} 
     
-**Total Queries:** {total_queries} 
-**Total Assistants:** {assistant}
-**Auto Leaving Assistant:** {ass}
-**Cleanmode duration:** {cm} Mins
-**Play Duration Limit:** {play_duration} Mins
-**Song Download Limit:** {song} Mins
-**Bot's Server Playlist Limit:** {playlist_limit}
-**Playlist Play Limit:** {fetch_playlist}"""
+**Toplam Sorgu:** {total_queries} 
+**Toplam Asistan:** {assistant}
+**Otomatik Ayrılma Yardımcısı:** {ass}
+**Temiz mod süresi:** {cm} Mins
+**Oynatma Süresi Sınırı:** {play_duration} Mins
+**Şarkı İndirme Sınırı:** {song} Mins
+**Bot'un Sunucu Oynatma Listesi Sınırı:** {playlist_limit}
+**Oynatma Listesi Oynatma Sınırı:** {fetch_playlist}"""
     med = InputMediaPhoto(media=config.STATS_IMG_URL, caption=text)
     try:
         await CallbackQuery.edit_message_media(
@@ -364,30 +364,30 @@ async def overall_stats(client, CallbackQuery, _):
     total_queries = await get_queries()
     blocked = len(BANNED_USERS)
     sudoers = len(await get_sudoers())
-    text = f""" **Bot's Stats and Information:**
-**Imported Modules:** {mod}
+    text = f""" **Bot'un İstatistikleri ve Bilgileri:**
+**İçe Aktarılan Modüller:** {mod}
 **Platform:** {sc}
 **Ram:** {ram}
-**Physical Cores:** {p_core}
-**Total Cores:** {t_core}
-**Cpu Frequency:** {cpu_freq}
-**Python Version :** {pyver.split()[0]}
-**Pyrogram Version :** {pyrover}
-**Py-TgCalls Version :** {pytgver}
-**Storage Avail:** {total[:4]} GiB
-**Storage Used:** {used[:4]} GiB
-**Storage Left:** {free[:4]} GiB
-**Served Chats:** {served_chats} 
-**Served Users:** {served_users} 
-**Blocked Users:** {blocked} 
-**Sudo Users:** {sudoers} 
-**Mongo Uptime:** {mongouptime[:4]} Days
-**Total DB Size:** {datasize[:6]} Mb
-**Total DB Storage:** {storage} Mb
-**Total DB Collections:** {collections}
-**Total DB Keys:** {objects}
-**Total DB Queries:** `{query}`
-**Total Bot Queries:** `{total_queries} `
+**Fiziksel Çekirdekler:** {p_core}
+**Toplam Çekirdek:** {t_core}
+**İşlemci Frekansı:** {cpu_freq}
+**Python Versiyonu :** {pyver.split()[0]}
+**Pyrogram Versiyonu :** {pyrover}
+**Py-TgCalls Sürümü :** {pytgver}
+**Depolama Kullanılabilir:** {total[:4]} GiB
+**Kullanılan Depolama:** {used[:4]} GiB
+**Depolama Solu:** {free[:4]} GiB
+**Sunulan Sohbetler:** {served_chats} 
+**Hizmet Verilen Kullanıcılar:** {served_users} 
+**Engellenmiş kullanıcılar:** {blocked} 
+**Sudo Kullanıcıları:** {sudoers} 
+**Mongo Çalışma Süresi:** {mongouptime[:4]} Days
+**Toplam Veritabanı Boyutu:** {datasize[:6]} Mb
+**Toplam Veritabanı Depolama Alanı:** {storage} Mb
+**Toplam Veritabanı Koleksiyonu:** {collections}
+**Toplam Veritabanı Anahtarları:** {objects}
+**Toplam Veritabanı Sorgusu:** `{query}`
+**Bottan Yapılan Sorgu Sayısı:** `{total_queries} `
     """
     med = InputMediaPhoto(media=config.STATS_IMG_URL, caption=text)
     try:
