b89119b10dfc73ba18ba2d75c2b5fad223693f50
melih022 <110200865+melih022@users.noreply.github.com>
2022-08-30 22:25:04 +0300

    Update speedtest.py

--- YukkiMusic/plugins/tools/speedtest.py
+++ YukkiMusic/plugins/tools/speedtest.py
@@ -32,20 +32,22 @@ def testspeed(m):
     except Exception as e:
         return m.edit(e)
     return result
-
-
 @app.on_message(filters.command(SPEEDTEST_COMMAND) & SUDOERS)
 async def speedtest_function(client, message):
-    m = await message.reply_text("BOT Hızı testi")
+    m = await message.reply_text("Running Speed test")
     loop = asyncio.get_event_loop()
     result = await loop.run_in_executor(None, testspeed, m)
-    output = f"""**Hız Testi Sonuçları**
+    output = f"""**Speedtest Results**
     
-<u>**SAHİP:**</u>
-**__Sahip:__**  @MissSahip [Mehmet]
-**__Sahip:__**  @YoutubeVcSahip [SupportPro]
-<u>**PİNG:**</u>
-**__Botumuz:__** @YoutubeVcProBot
+<u>**Client:**</u>
+**__ISP:__** {result['client']['isp']}
+**__Country:__** {result['client']['country']}
+  
+<u>**Server:**</u>
+**__Name:__** {result['server']['name']}
+**__Country:__** {result['server']['country']}, {result['server']['cc']}
+**__Sponsor:__** {result['server']['sponsor']}
+**__Latency:__** {result['server']['latency']}  
 **__Ping:__** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, 
@@ -53,3 +55,5 @@ async def speedtest_function(client, message):
         caption=output
     )
     await m.delete()
+
+
