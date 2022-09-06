c504cb9f2f4c3bc63fd730e71c6dbee4c9b9a382
melih022 <110200865+melih022@users.noreply.github.com>
2022-09-06 14:57:56 +0300

    Update stream.py

--- YukkiMusic/utils/stream/stream.py
+++ YukkiMusic/utils/stream/stream.py
@@ -115,12 +115,11 @@ async def stream(
                 )
                 img = await gen_thumb(vidid)
                 button = stream_markup(_, vidid, chat_id)
-                run = await app.send_photo(
+                run = await app.send_message(
                     original_chat_id,
-                    photo=img,
-                    caption=_["stream_1"].format(
+                    _["stream_1"].format(
                         user_name,
-                        f"https://t.me/{app.username}?start=info_{vidid}",
+                        f"https://t.me/youtubevcprobot?start=info_{vidid}",
                     ),
                     reply_markup=InlineKeyboardMarkup(button),
                 )
@@ -139,9 +138,8 @@ async def stream(
                 car, randint(100, 10000000)
             )
             upl = close_markup(_)
-            return await app.send_photo(
+            return await app.send_message(
                 original_chat_id,
-                photo=carbon,
                 caption=_["playlist_18"].format(link, position),
                 reply_markup=upl,
             )
@@ -196,10 +194,9 @@ async def stream(
             )
             img = await gen_thumb(vidid)
             button = stream_markup(_, vidid, chat_id)
-            run = await app.send_photo(
+            run = await app.send_message(
                 original_chat_id,
-                photo=img,
-                caption=_["stream_1"].format(
+                _["stream_1"].format(
                     user_name,
                     f"https://t.me/{app.username}?start=info_{vidid}",
                 ),
@@ -249,9 +246,8 @@ async def stream(
                 forceplay=forceplay,
             )
             button = telegram_markup(_, chat_id)
-            run = await app.send_photo(
+            run = await app.send_message(
                 original_chat_id,
-                photo=config.SOUNCLOUD_IMG_URL,
                 caption=_["stream_3"].format(
                     title, duration_min, user_name
                 ),
@@ -305,11 +301,8 @@ async def stream(
             if video:
                 await add_active_video_chat(chat_id)
             button = telegram_markup(_, chat_id)
-            run = await app.send_photo(
+            run = await app.send_message(
                 original_chat_id,
-                photo=config.TELEGRAM_VIDEO_URL
-                if video
-                else config.TELEGRAM_AUDIO_URL,
                 caption=_["stream_4"].format(
                     title, link, duration_min, user_name
                 ),
@@ -365,10 +358,9 @@ async def stream(
             )
             img = await gen_thumb(vidid)
             button = telegram_markup(_, chat_id)
-            run = await app.send_photo(
+            run = await app.send_message(
                 original_chat_id,
-                photo=img,
-                caption=_["stream_1"].format(
+                _["stream_1"].format(
                     user_name,
                     f"https://t.me/{app.username}?start=info_{vidid}",
                 ),
@@ -418,9 +410,8 @@ async def stream(
                 forceplay=forceplay,
             )
             button = telegram_markup(_, chat_id)
-            run = await app.send_photo(
+            run = await app.send_message(
                 original_chat_id,
-                photo=config.STREAM_IMG_URL,
                 caption=_["stream_2"].format(user_name),
                 reply_markup=InlineKeyboardMarkup(button),
             )
