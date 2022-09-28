a1ddd62eb9e7032425daf5c1f8a0299a38fbfe48
melih022 <110200865+melih022@users.noreply.github.com>
2022-09-28 06:39:37 +0300

    Update playlist.py

--- YukkiMusic/utils/inline/playlist.py
+++ YukkiMusic/utils/inline/playlist.py
@@ -31,36 +31,6 @@ def botplaylist_markup(_):
     ]
     return buttons
 
-
-def top_play_markup(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["PL_B_9"], callback_data="SERVERTOP global"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["PL_B_10"], callback_data="SERVERTOP chat"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["PL_B_11"], callback_data="SERVERTOP user"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["BACK_BUTTON"], callback_data="get_playmarkup"
-            ),
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"], callback_data="close"
-            ),
-        ],
-    ]
-    return buttons
-
-
 def get_playlist_markup(_):
     buttons = [
         [
@@ -74,35 +44,6 @@ def get_playlist_markup(_):
         [
             InlineKeyboardButton(
                 text=_["BACK_BUTTON"], callback_data="home_play"
-            ),
-            InlineKeyboardButton(
-                text=_["CLOSE_BUTTON"], callback_data="close"
-            ),
-        ],
-    ]
-    return buttons
-
-
-def top_play_markup(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["PL_B_9"], callback_data="SERVERTOP Global"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["PL_B_10"], callback_data="SERVERTOP Group"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["PL_B_11"], callback_data="SERVERTOP Personal"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["BACK_BUTTON"], callback_data="get_playmarkup"
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
