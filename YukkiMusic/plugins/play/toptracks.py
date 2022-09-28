e4f8554dba2a0b47fb0948fe38a480a39eb192a8
melih022 <110200865+melih022@users.noreply.github.com>
2022-09-28 06:56:37 +0300

    Update toptracks.py

--- YukkiMusic/plugins/play/toptracks.py
+++ YukkiMusic/plugins/play/toptracks.py
@@ -18,8 +18,7 @@ from YukkiMusic.utils.database import (get_global_tops,
                                        get_particulars, get_userss)
 from YukkiMusic.utils.decorators.language import languageCB
 from YukkiMusic.utils.inline.playlist import (botplaylist_markup,
-                                              failed_top_markup,
-                                              top_play_markup)
+                                              failed_top_markup)
 from YukkiMusic.utils.stream.stream import stream
 
 loop = asyncio.get_running_loop()
