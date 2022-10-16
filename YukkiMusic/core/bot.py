0f9ae4090c5e3a1c8464c93ea0c59640d7a36488
melih022 <110200865+melih022@users.noreply.github.com>
2022-10-16 04:46:16 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -19,7 +19,7 @@ from ..logging import LOGGER
 
 class YukkiBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
+        LOGGER(__name__).info(f"Bot Aktif")
         super().__init__(
             "YukkiMusicBot",
             api_id=config.API_ID,
@@ -34,26 +34,26 @@ class YukkiBot(Client):
         self.id = get_me.id
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "Bot Started"
+                config.LOG_GROUP_ID, "Bot Aktif"
             )
         except:
             LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                "Bot, günlük Grubuna erişemedi. Botunuzu günlük kanalınıza eklediğinizden ve yönetici olarak terfi ettiğinizden emin olun.!"
             )
             sys.exit()
         if config.SET_CMDS == str(True):
             try:
                 await self.set_bot_commands(
                     [
-                        BotCommand("ping", "Check that bot is alive or dead"),
-                        BotCommand("play", "Starts playing the requested song"),
-                        BotCommand("skip", "Moves to the next track in queue"),
-                        BotCommand("pause", "Pause the current playing song"),
-                        BotCommand("resume", "Resume the paused song"),
-                        BotCommand("end", "Clear the queue and leave voice chat"),
-                        BotCommand("shuffle", "Randomly shuffles the queued playlist."),
-                        BotCommand("playmode", "Allows you to change the default playmode for your chat"),
-                        BotCommand("settings", "Open the settings of the music bot for your chat.")
+                        BotCommand("ping", "Botun Aktif olup olmadığını gösterir."),
+                        BotCommand("oynat", "Sarkı oynatır"),
+                        BotCommand("atla", "Akış Değisir"),
+                        BotCommand("durdur", "akısı durdurur."),
+                        BotCommand("devam", "Akısı Devam ettirir"),
+                        BotCommand("son", "Sonlandırır ve Listesi temizler."),
+                        BotCommand("karistir", "listeyi random olarak calar."),
+                        BotCommand("oynatmodu", "Ayarlar Yapabilirsin"),
+                        BotCommand("ayarlar", "Bot genel Ayarları icin.")
                         ]
                     )
             except:
@@ -63,11 +63,11 @@ class YukkiBot(Client):
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
             LOGGER(__name__).error(
-                "Please promote Bot as Admin in Logger Group"
+                "Lütfen Bot'u Log Groubunda Yönetici Yapın"
             )
             sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        LOGGER(__name__).info(f"MusicBot Olarak basladi {self.name}")
